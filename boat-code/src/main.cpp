f11991d0dedef926124af28266289a578aca4786
EmithU <eu52@cornell.edu>
2026-01-17 13:27:05 -0500

    implemented xbee toggleable relays

--- boat-code/src/main.cpp
+++ boat-code/src/main.cpp
@@ -1,18 +1,61 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+SoftwareSerial xbee(2, 3); 
+
+const int relay_12v  = 7; // red
+const int relay_5v   = 6; // green
+const int relay_vnav = 5; // blue
+const int relay_3v   = 4; // yellow
+
+bool relay_12v_state  = true;
+bool relay_5v_state   = true;
+bool relay_vnav_state = true;
+bool relay_3v_state   = true;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  xbee.begin(9600); 
+
+  pinMode(relay_12v , OUTPUT);
+  pinMode(relay_5v  , OUTPUT);
+  pinMode(relay_vnav, OUTPUT);
+  pinMode(relay_3v,   OUTPUT);
+
+  // Init to off state
+  digitalWrite(relay_12v , relay_12v_state );
+  digitalWrite(relay_5v  , relay_5v_state  );
+  digitalWrite(relay_vnav, relay_vnav_state);
+  digitalWrite(relay_3v  , relay_3v_state  );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Read from XBee
+  if (xbee.available()) {
+    char data = xbee.read();
+
+    switch (data) {
+    case '1':
+        relay_12v_state = !relay_12v_state;
+        break;
+    case '2':
+        relay_5v_state = !relay_5v_state;
+        break;
+    case '3':
+        relay_vnav_state = !relay_vnav_state;
+        break;
+    case '4':
+        relay_3v_state = !relay_3v_state;
+        break;
+    }
+  }
+
+  // Set relay states
+  digitalWrite(relay_12v , relay_12v_state );
+  digitalWrite(relay_5v  , relay_5v_state  );
+  digitalWrite(relay_vnav, relay_vnav_state);
+  digitalWrite(relay_3v  , relay_3v_state  );
+
+  // Write to XBee
+  //xbee.print('a'); // Send to other XBee
 }
